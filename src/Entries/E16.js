80764205de8816d5b6ebec67953b004acbe9d037
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2022-03-09 22:28:03 -0600

    yep

--- src/Entries/E16.js
+++ src/Entries/E16.js
@@ -3,18 +3,12 @@ import React from 'react';
 function E16(){
     return(
         <div>
-            <h1>3.2.22</h1>
-<<<<<<< HEAD
+           <h1>3.2.22</h1>
             <span>"Similarly, do not judge monks as deserving of respect or as being worthless, nor pay attention to whether a person has been practicing for only a short time or 
                 for many years. Without knowing where to find our own stability, how are we to know where somelese else would be most stable? If the standard with which we evaluate others is incorrect,
                 we are likely to see their good points as bad, and vice versa. What a mistake to make!"
             </span>
-            <iframe src="https://open.spotify.com/embed/track/6MCjmGYlw6mQVWRFVgBRvB?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>      
-=======
-            <span>Oooooo I wanna koi fish tattoo rn. I think it would look pretty snazzy
-            </span>
-            <iframe src="https://open.spotify.com/embed/track/3vySEUpD0tc801F2WZDLYw?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
->>>>>>> 9e3afc402e3603976bec99383afa77418f351c5b
+            <iframe src="https://open.spotify.com/embed/track/6MCjmGYlw6mQVWRFVgBRvB?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
         </div>     
     )
 }
--- src/Entries/E23.js
+++ src/Entries/E23.js
@@ -4,8 +4,9 @@ function E23(){
     return(
         <div>
             <h1>3.9.22</h1>
-            <span>TBD
+            <span>I hate being jealous of other people's happiness. Focus on yourself!
             </span>
+            <iframe src="https://open.spotify.com/embed/track/6ieWL5CLN9WdC875guWtMe?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
         </div>
     )}
 
