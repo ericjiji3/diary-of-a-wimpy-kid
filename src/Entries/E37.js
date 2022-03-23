98504dd64e755bb45fbb1e0a10912a262c2bde6c
ericjiji3 <eric.ji3333@gmail.com>
2022-03-23 08:58:35 -0500

    mannn

---
+++ src/Entries/E37.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function E37(){
+    return(
+        <div>
+            <h1>3.23.22</h1>
+            <iframe src="https://open.spotify.com/embed/track/0d8anwJJGti8jE79Y4tXUD?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+        </div>
+    )}
+
+export default E37;
