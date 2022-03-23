ee5df51bf2772364d51c140f9eb178bbd6ad3148
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2022-03-22 23:44:53 -0500

    another L today too many Ls

--- src/Entries/E36.js
+++ src/Entries/E36.js
@@ -4,8 +4,11 @@ function E36(){
     return(
         <div>
             <h1>3.22.22</h1>
-            <span>
+            <span>"I'm stuck in my ways and its aAalLl because of women"
             </span>
+            <h2></h2>
+            <span>"Tried to steal my own miIinNdD away from meditaAaAAatTiIion"</span>
+            <iframe src="https://open.spotify.com/embed/track/0d8anwJJGti8jE79Y4tXUD?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
         </div>
     )}
 
