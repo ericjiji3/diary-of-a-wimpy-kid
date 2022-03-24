05e8ec47771d152f6e3ccdb2852e863ee82221e2
ericjiji3 <eric.ji3333@gmail.com>
2022-03-24 09:53:21 -0500

    first order ofbidness

--- src/Entries/E38.js
+++ src/Entries/E38.js
@@ -5,6 +5,10 @@ function E38(){
         <div>
             <h1>3.24.22</h1>
             <span>"Its your compulsive reaction to life which hurts you, not what other people are doing"</span>
+            <h2></h2>
+            <span>Theres a huge difference between reaction and response- unconscious vs conscious. If you live to react to others, you are a slave to other's opinions. Being able to consciously respond to others means that you reach into your 
+                inner self rather than be driven by instant beliefs or biases. Reaction is to be emotional, response is to have emotional intelligence.
+            </span>
             <iframe src="https://open.spotify.com/embed/track/615XzGXe72YUjagvkMmEFi?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
         </div>
     )}
